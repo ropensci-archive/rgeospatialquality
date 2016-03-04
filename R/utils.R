2e1fe2238be0207b5be9feec354202a930e9277d
Javier Otegui <javier.otegui@gmail.com>
2016-03-04 11:26:00 +0100

    Added main documentation in README.md

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 BASE_URL <- "http://api-geospatial.vertnet-portal.appspot.com/geospatial"
 
 gq_parse <- function(req) {
-    return(jsonlite::fromJSON(httr::content(req, as="text"), simplifyVector=TRUE))
+    return(jsonlite::fromJSON(httr::content(req, as="text", encoding="UTF-8"), simplifyVector=TRUE))
 }
