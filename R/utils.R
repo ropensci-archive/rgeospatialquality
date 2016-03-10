06ce3d0ec61ea3d9bc376f1e5894ebfd17e1852c
Javier Otegui <javier.otegui@gmail.com>
2016-03-10 11:28:27 +0100

    Added generic handler for 4XX and 5XX errors

--- R/utils.R
+++ R/utils.R
@@ -7,5 +7,10 @@ gq_check_df <- function(indf) {
 }
 
 gq_parse <- function(req) {
-    return(jsonlite::fromJSON(httr::content(req, as="text", encoding="UTF-8"), simplifyVector=TRUE))
+    if (req$status_code<400) {
+        return(jsonlite::fromJSON(httr::content(req, as="text", encoding="UTF-8"), simplifyVector=TRUE))
+    } else {
+        err_msg <- c("Something went wrong with the call. Got status ", req$status_code, ": ", httr::content(req, as="text"))
+        stop(err_msg)
+    }
 }
