3a53c5153fe13df02aee0e7a8de8bb25e9c83d1c
Javier Otegui <javier.otegui@gmail.com>
2016-04-08 12:13:42 +0200

    Implemented add_flags and format_gq as S3 classes. Closes #15

--- R/add_flags.R
+++ R/add_flags.R
@@ -81,7 +81,12 @@
 #'
 #' @export
 add_flags <- function(indf, guess_fields=FALSE, show_summary=TRUE, quiet=FALSE, ...) {
-
+    UseMethod("add_flags", indf)
+}
+add_flags.default <- function(indf, guess_fields=FALSE, show_summary=TRUE, quiet=FALSE, ...) {
+    stop("Only data.frame objects are currently accepted", call. = FALSE)
+}
+add_flags.data.frame <- function(indf, guess_fields=FALSE, show_summary=TRUE, quiet=FALSE, ...) {
     # Parse input
     indf2 <- gq_parse_dataframe(indf, guess_fields, quiet)
 
@@ -116,7 +121,7 @@ gq_replace_field <- function(indf, orig, dwc, quiet) {
 
 gq_parse_dataframe <- function(indf, guess_fields=FALSE, quiet=FALSE) {
 
-    # Parse input object type
+    # Parse input
     gq_check_df(indf)
 
     # Stop if too manu records.
--- R/format_gq.R
+++ R/format_gq.R
@@ -75,8 +75,14 @@
 #'
 #' @export
 format_gq <- function(indf, source=NULL, config=NULL, quiet=FALSE, ...) {
+    UseMethod("format_gq", indf)
+}
+format_gq.default <- function(indf, ...) {
+    stop("Only data.frame objects are currently accepted", call. = FALSE)
+}
+format_gq.data.frame <- function(indf, source=NULL, config=NULL, quiet=FALSE, ...) {
 
-    # Parse input object type
+    # Parse input
     gq_check_df(indf)
 
     # Mapping via 'source'
